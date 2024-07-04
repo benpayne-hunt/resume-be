929db8d57abcfef79c6a7e51e75c9df1dc891e0f
Benjamin Payne-Hunt <ben@benpaynehunt.co.uk>
2024-07-04 17:34:24 +0100

    Adds functioning cors

--- node/src/index.ts
+++ node/src/index.ts
@@ -1,13 +1,23 @@
-import express, { Express, Request, Response } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express";
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
+app.use(cors());
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+
+app.get("/", cors(corsOptions), (req: Request, res: Response) => {
+  res.send({ ok: true, data: "Express + TypeScript Server" });
 });
 
 app.listen(port, () => {
