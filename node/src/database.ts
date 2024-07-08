a21a3bc66c74d77d8b0bc3345cb779c90be25b69
Ben Payne Hunt <ben@benpaynehunt.co.uk>
2024-07-08 17:26:28 +0100

    Refactors to make two working endpoints

--- node/src/database.ts
+++ node/src/database.ts
@@ -3,7 +3,9 @@ import mongoose from "mongoose";
 const connect = async () => {
   const MONGO_URI = process.env.MONGO_URI!;
 
-  mongoose.connect(MONGO_URI);
+  mongoose.connect(MONGO_URI, {
+    dbName: "resume",
+  });
 };
 
 export default connect;
--- node/src/index.ts
+++ node/src/index.ts
@@ -2,30 +2,49 @@ import cors from "cors";
 import dotenv from "dotenv";
 import express, { Express, Request, Response } from "express";
 
-import { getSkillsSearchCodeSight } from "./Domains/codeSight/controllers/SkillsSearchController";
 import connect from "./database";
+import { getSkillsSearchCodeSight } from "./domains/codeSight/controllers/skillsSearchController";
+import { allSkills, findSkill } from "./domains/skills/controllers/findSkillsController";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 8000;
 
-app.use(cors());
-
 const corsOptions = {
   origin: "http://localhost:3000",
 };
 
 app.use(cors(corsOptions));
+app.use(express.json());
 
+// Connect to the database
 connect();
 
-// Ping to check if the server is up
+/**
+ * Ping to check if the server is up
+ */
 app.get("/ping", cors(corsOptions), (req: Request, res: Response) => {
   res.send({ ok: true, data: "ok" });
 });
 
-// Code Sight Routes
+/**
+ * All Skills
+ */
+app.get("/skills", cors(corsOptions), async (req: Request, res: Response) => {
+  res.json(await allSkills());
+});
+/**
+ * Find Skill
+ */
+app.post("/skill", cors(corsOptions), async (req: Request, res: Response) => {
+  res.send(await findSkill(req.body.name));
+});
+
+/**
+ * Code Sight Skills Search
+ */
 app.get("/code-sight/skills-search", cors(corsOptions), (req: Request, res: Response) => {
   res.send(getSkillsSearchCodeSight()).status(200);
 });
